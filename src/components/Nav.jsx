f206a05a35914d1367a38aa376b4b61be78a1eff
Marisi Romanillos <marisiromanillos@Marisis-MBP.lan>
2023-07-12 10:52:24 +0100

    nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ const Nav = (props) => {
   return (
     <nav>
       <div className="logo-container">
-        <img className="logo" src={"./gymMate.png"} alt="" />
+        <img className="logo" src={"./gymMate.png"} alt="gym mate logo" />
       </div>
       {!props.authToken && <button className="nav-btn">Log in</button>}
     </nav>
