957a1ba758bac0901aa63f48df2eb6fca7d1aa59
Marisi Romanillos <marisiromanillos@Marisis-MBP.lan>
2023-07-12 11:02:30 +0100

    creating authModal

---
+++ src/components/AuthModal.jsx
@@ -0,0 +1,13 @@
+const AuthModal = () => {
+  const handleClick = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      <div className="" onClick={handleClick}></div>
+    </div>
+  );
+};
+
+export default AuthModal;
